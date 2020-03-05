e4a0d81b9679b8e50a09e3d63a4cac14219cff62
Josh Newman <josh.newman4390@gmail.com>
2020-03-05 20:29:56 +1000

    initial commit w/ todo notes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const timeSigStyle = {
+  fontSize: "60px"
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          marginTop: "100px",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column"
+        }}
+      >
+        <div>
+          <svg id="beat" width="900px">
+            {/* <!-- start bar --> */}
+            <line
+              x1="0"
+              y1="0"
+              x2="0"
+              y2="80"
+              stroke="#000"
+              stroke-width="10"
+            />
+
+            {/* <!-- time signature --> */}
+            <text style={timeSigStyle} x="20" y="40">
+              4
+            </text>
+            <text style={timeSigStyle} x="20" y="80">
+              4
+            </text>
+
+            {/* <!-- bar lines -->
+          <!-- time sig takes up 60 width --> */}
+            <line x1="0" y1="0" x2="900" y2="0" stroke="#000" />
+            <line x1="0" y1="20" x2="900" y2="20" stroke="#000" />
+            <line x1="0" y1="40" x2="900" y2="40" stroke="#000" />
+            <line x1="0" y1="60" x2="900" y2="60" stroke="#000" />
+            <line x1="0" y1="80" x2="900" y2="80" stroke="#000" />
+
+            {/* <!-- end bar --> */}
+            <line
+              x1="900"
+              y1="0"
+              x2="900"
+              y2="80"
+              stroke="#000"
+              stroke-width="10"
+            />
+          </svg>
+        </div>
+      </div>
     </div>
   );
 }
